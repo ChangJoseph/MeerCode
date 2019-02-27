738b06875e92695fb2342d100bc6ab4b80f0c72c
Joseph Chang <niceinho@gmail.com>
2019-02-27 12:57:04 -0500

    Made parser into a util style class

--- src/main/java/meercode/parser/Parser.java
+++ src/main/java/meercode/parser/Parser.java
@@ -3,7 +3,7 @@ package meercode.parser;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Parser {
+public final class Parser {
 
     List<List<String>> mTokens;
 
@@ -14,12 +14,17 @@ public class Parser {
         tokens.get(0).add("test 0 1");
         tokens.add(new ArrayList<String>());
         tokens.get(1).add("test 1 0");
-        Parser parser = new Parser(tokens);
+
+        AbstractSyntaxTree ast = Parser.parseTokens(tokens);
     }
 
-    public Parser(List<List<String>> pTokens) {
-
+    private Parser() {
+        throw Exception;
     }
 
-    public static Abstract
+    public static AbstractSyntaxTree parseTokens(List<List<String>> pTokens) {
+        AbstractSyntaxTree tree = new AbstractSyntaxTree();
+        //Parse here
+        return tree;
+    }
 }
