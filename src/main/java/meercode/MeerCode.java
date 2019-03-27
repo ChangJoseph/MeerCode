19c8b658c87c5e0cbb9a527df975f1377a5491bf
Joseph Chang <niceinho@gmail.com>
2019-03-27 15:34:23 -0400

    Added basic integration

--- src/main/java/meercode/MeerCode.java
+++ src/main/java/meercode/MeerCode.java
@@ -3,13 +3,22 @@
  */
 package meercode;
 
-// import meercode.*;
+import meercode.lexer.*;
+
+import java.util.List;
+
+import meercode.ast.*;
+import meercode.parser.*;
 
 public class MeerCode {
     
     public static void main(String[] args)
     {
-        
-        
+        String filePath3AC = "";
+        String codeFilePath = "";
+        List<List<String>> tokens = Lexer.getTokenList(codeFilePath);
+        AbstractSyntaxTree ast = Parser.parseTokens(tokens);
+        ASTReader.convertTo3AC(ast, filePath3AC);
+        Compiler.compile(filePath3AC);
     }
 }
